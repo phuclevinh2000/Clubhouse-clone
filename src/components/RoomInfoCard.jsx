ac3b7cf19e43b7a1230b54fd08d4f6adc792de17
Phuc Le <phuclevinh2000@gmail.com>
2021-09-28 01:34:49 +0300

    add content part for home page

--- src/components/RoomInfoCard.jsx
+++ src/components/RoomInfoCard.jsx
@@ -1,12 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import style from "../style/roomCard.module.css"
 import data from "../data/roomCard.json"
+import { BsChatDots, BsFillPersonFill, BsChatDotsFill } from "react-icons/bs"
+import { FaBeer } from 'react-icons/fa';
 
 const RoomInfoCard = () => {
-    console.log(data)
+    // console.log(data)
     return (
         <div>
-            
+            {data.map((item) => (
+                <div>
+                    <div>
+                        <div className={style.roomCardContainer}>
+                            <h6>{item.title}</h6>
+                            <h2>{item.sub_title}</h2>
+                            <div className={style.roomMembers}>
+                                <div>
+                                    <img src="/images/user_image_1.jpg" alt="" />
+                                    <img src="/images/user_image_2.jpg" alt="" />
+                                </div>
+                                <div>
+                                    {item.members.map((person) => (
+                                        <p>
+                                            {person.first_name} {person.last_name} <BsChatDots /> 
+                                        </p>
+                                    ))}
+                                    <p className="d-flex align-items-center">
+                                        <span className="mr-2">1.8</span>
+                                        <BsFillPersonFill />
+                                        <span className="mx-2"></span>{" "}
+                                        <span className="mr-2">5</span> <BsChatDotsFill />
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Header from '../components/Header'
 import style from "../style/home.module.css"
 import DailyInfoCard from "../components/DailyInfoCard"
 import RoomInfoCard from '../components/RoomInfoCard'
+import { AiOutlinePlus } from "react-icons/ai"
+import { BsGrid3X3Gap } from "react-icons/bs"
 
 const Home = () => {
     return (
@@ -11,6 +13,15 @@ const Home = () => {
             <div className={style.home_container}>
                 <DailyInfoCard />
                 <RoomInfoCard />
+            </div>
+            <div className={style.action_btn}>
+                <button>
+                    <AiOutlinePlus className="mr-2"/>
+                    Start a room
+                </button>
+                <button>
+                    <BsGrid3X3Gap />
+                </button>
             </div>
         </>
     )
