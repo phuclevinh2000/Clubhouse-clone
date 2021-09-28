e47a45da49b5c4a78025264d7275ac2970abadb7
Phuc Le <phuclevinh2000@gmail.com>
2021-09-29 00:40:18 +0300

    adding style and content for swipeup button

--- src/components/bottom_sheets/StartRoom.jsx
+++ src/components/bottom_sheets/StartRoom.jsx
@@ -2,14 +2,60 @@ import React, {useState} from 'react'
 import style from "../../style/bottomSheet.module.css"
 import { FcGlobe } from "react-icons/fc"
 
-const StartRoom = () => {
+const StartRoom = (props) => {
     const [room, setRoom] = useState("open")
     return (
         <>
-            <div className={style.switch_line}></div>
-            <div className="text-right">
+            <div className={style.switch_Line}></div>
+            <div className="text-end">
                 <button className={style.addTopicBtn}>+ Add a topic</button>
-                
+            </div>
+            <div className={style.selectRoom}>
+                <button 
+                    className={room === "open" ? style.active : ""}
+                    onClick={() => setRoom("open")}
+                >
+                    <div>
+                        <FcGlobe style={{fontSize: '70'}}/>
+                    </div>
+                    Open
+                </button>
+                <button 
+                    className={room === 'social' ? style.active : ""}
+                    onClick={() => setRoom("social")}
+                >
+                    <div>
+                        <FcGlobe style={{fontSize: '70'}}/>
+                    </div>
+                    Social
+                </button>
+                <button 
+                    className={room === 'closed' ? style.active : ""}
+                    onClick={() => setRoom("closed")}
+                >
+                    <div>
+                        <FcGlobe style={{fontSize: '70'}}/>
+                    </div>
+                    Closed
+                </button>
+            </div>
+            <p>Start a room {" "}
+                <span>{
+                    room ==="closed" ? "for people I choose" :
+                    room ==="social" ? "with people I follow" 
+                    : "open to everyone"}
+                </span>
+            </p>
+            <div className="text-center">
+                <button 
+                    className={style.letGoBtn}
+                    onClick={() => {
+                        props.setSheetCreateRoom(true)
+                        props.setSheetVisible(true)
+                    }}
+                >
+                    Let's go
+                </button>
             </div>
         </>
     )
