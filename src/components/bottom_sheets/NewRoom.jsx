7f2687b1cc476d22da7021f83c93d7126f157198
Phuc Le <phuclevinh2000@gmail.com>
2021-10-01 22:53:08 +0300

    create chatroom

--- src/components/bottom_sheets/NewRoom.jsx
+++ src/components/bottom_sheets/NewRoom.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 import style from "../../style/roomDetail.module.css"
-import { AiOutlineFile } from 'react-icons/ai'
-import { BsMicMuteFill } from 'react-icons/bs'
+import { AiOutlineFile, AiOutlinePlus } from 'react-icons/ai'
+import { BsMicMuteFill, BsMicFill } from 'react-icons/bs'
 
 const NewRoom = (props) => {
     const [micMuteVisible, setMicMuteVisible] = useState(false)
+    const [ itemVisible, setItemVisible] = useState(true)
     const card = props.cardDetail
 
     return (
@@ -13,11 +14,71 @@ const NewRoom = (props) => {
                 <div className={style.head}>
                     <div className="d-flex align-items-center">
                         <a 
-                            href="google.com" 
+                            href="#" 
                             onClick={() => props.setSheetVisible(false)} 
                         >
-                            <img src="/images/arrow.png" alt="" />
+                            <img className={style.arrow_icon} src="/images/arrow.png" alt="" />
                         </a>
+                        <span>Hallway</span>
+                    </div>
+                    <div>
+                        <AiOutlineFile />
+                        <img 
+                            src="/images/user_image_2.jpg"
+                            alt="" 
+                            className={style.profile_img}    
+                        />
+                    </div>
+                </div>
+                <div className={style.roomDetailCard}>
+                    <div    
+                        className="d-flex align-items-center justify-content-between flex-wrap"
+                        style={{ padding: "0.5em 1em"}}
+                    >
+                        {card.members.map((item) => (
+                            <div className={style.memberContainer}>
+                                {micMuteVisible ? (
+                                    <div className={style.audio_icon}>
+                                        <BsMicMuteFill />
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                                <img 
+                                    src="/images/user_image_2.jpg"
+                                    alt="" 
+                                />
+                                {/* {console.log(item.profile_img)} */}
+                                <p>
+                                    <span>*</span>
+                                    {item.first_name}
+                                    
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={style.footer}>
+                    <button
+                        onClick={() => {
+                            props.setSheetVisible(false)
+                        }}
+                    >
+                        <img src="/images/handPeace.png" alt="" /> 
+                        Leave Quietly
+                    </button>
+                    <div>
+                        <button>
+                            <AiOutlinePlus />
+                        </button>
+                        <button>
+                            <img src="/images/stopHand.png" alt="" />
+                        </button>
+                        <button 
+                            onClick={() => setMicMuteVisible(!micMuteVisible)}
+                        >
+                            {micMuteVisible ? <BsMicMuteFill /> : <BsMicFill />}
+                        </button>
                     </div>
                 </div>
             </div> 
